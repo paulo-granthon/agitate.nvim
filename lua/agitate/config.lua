a7488c30c1fc511ffcf588e33ff852a888f4313f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-02 14:29:25 -0300

    fix: remove unnecessary prints in `config`

--- lua/agitate/config.lua
+++ lua/agitate/config.lua
@@ -10,8 +10,6 @@ M.options = {}
 
 ---@param options Config|nil
 function M.setup(options)
-    print('config.setup')
-    print(options)
     M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
 end
 
