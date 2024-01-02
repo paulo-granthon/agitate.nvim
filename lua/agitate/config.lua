fedbec06a83d0a97979a84ba6e60f92d165b024a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-02 15:43:08 -0300

    wip: debug

--- lua/agitate/config.lua
+++ lua/agitate/config.lua
@@ -13,11 +13,13 @@ M.options = {}
 
 ---@param options Config|nil
 function M.setup(options)
+    print('config.setup monstro')
     M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
 end
 
 ---@param options Config|nil
 function M.extend(options)
+    print('config.extend monstro')
     M.options = vim.tbl_deep_extend("force", {}, M.options or defaults, options or {})
 end
 
--- lua/agitate/init.lua
+++ lua/agitate/init.lua
@@ -5,12 +5,14 @@ local M = {}
 
 ---@param opts Config|nil
 function M.load(opts)
+    print('load monstro')
     if opts then
         config.extend(opts)
     end
 end
 
 function M.setup()
+    print('setup monstro')
     config.setup()
     repo.setup()
 end
