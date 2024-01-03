b8bc62419f528adc964c5fe215109784e2682f6f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 17:30:50 -0300

    feat: allow `fargs` in `AgitateRepoInitGitHub` user command

--- lua/agitate/api/repo.lua
+++ lua/agitate/api/repo.lua
@@ -15,10 +15,13 @@ function M.setup()
 
     vim.api.nvim_create_user_command(
         'AgitateRepoInitGitHub',
-        function()
-            require('agitate.core.repo').InitGitHub()
+        function(opts)
+            require('agitate.core.repo').InitGitHub(
+                opts.fargs and opts.fargs[1] or nil
+            )
         end, {
-            desc = 'Initialize the current directory as a repository at \'github.com/github_username/current_directory\'',
+            nargs = '?',
+            desc = 'Initialize the current directory as a repository at \'github.com/<github_username>/<argument or current_directory>/\'',
         }
     )
 end
