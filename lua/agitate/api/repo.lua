3df2a7a0e6d96b169967cddc86210313ad7d7605
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 17:10:48 -0300

    feat: add `user_command` for `CreateGitHubCurl` function of `core.repo`

--- lua/agitate/api/repo.lua
+++ lua/agitate/api/repo.lua
@@ -2,6 +2,16 @@ local M = {}
 
 function M.setup()
     vim.api.nvim_create_user_command(
+        'AgitateRepoCreateGitHub',
+        function(fargs)
+            require('agitate.core.repo').CreateGitHubCurl(fargs)
+        end, {
+            nargs = '?',
+            desc = 'Creates a new repository at \'github.com/<github_username>/<argument or current_directory>/\'',
+        }
+    )
+
+    vim.api.nvim_create_user_command(
         'AgitateRepoInitGitHub',
         function()
             require('agitate.core.repo').InitGitHub()
