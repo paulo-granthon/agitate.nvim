f748e8ecbad852601910bf60027ee65fd3bcb171
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 00:08:53 -0300

    fix: module path of `InitGitHub` user_command in `setup` function of `core.repo`

--- lua/agitate/core/repo.lua
+++ lua/agitate/core/repo.lua
@@ -26,7 +26,7 @@ end
 -- Create vim commands
 function M.setup()
     vim.api.nvim_create_user_command('AgitateRepoInitGitHub', function()
-        require('agitate.repo').InitGitHub()
+        require('agitate.core.repo').InitGitHub()
     end, {
         desc = 'Initializes the current directory as a repository at \'github.com/github_username/current_directory\'',
     })
