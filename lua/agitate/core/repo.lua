64ebaf402fdb357537dc6ff8a0b80f1f4fd322bc
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 16:57:01 -0300

    feat: add `optional_repo_name` parameter to `InitGitHub` in `core.repo`

--- lua/agitate/core/repo.lua
+++ lua/agitate/core/repo.lua
@@ -26,18 +26,18 @@ function M.CreateGitHubCurl(optional_repo_name)
 end
 
 -- Initialize a new repository and push to GitHub
-function M.InitGitHub()
+function M.InitGitHub(optional_repo_name)
     local options = require('agitate.config').options
 
-    local directory_name = util.get_directory_name()
+    local github_repository_name = optional_repo_name or util.get_directory_name()
     local github_username = options.github_username
 
-    util.execute_command('echo "# ' .. directory_name .. '" >> README.md')
+    util.execute_command('echo "# ' .. github_repository_name .. '" >> README.md')
     vim.cmd('G init')
     vim.cmd('G add README.md')
     vim.cmd('G commit -m "first commit"')
     vim.cmd('G branch -M main')
-    vim.cmd('G remote add origin https://github.com/' .. github_username .. '/' .. directory_name .. '.git')
+    vim.cmd('G remote add origin https://github.com/' .. github_username .. '/' .. github_repository_name .. '.git')
     vim.cmd('G push -u origin main')
 
     -- Open fugitive status window
