15c599f9e9bcf4f88db526bcd50fd90fe0ccc04a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-02 23:53:13 -0300

    feat: `CreateCheckoutAndPush` function in `core.branch`

--- lua/agitate/core/branch.lua
+++ lua/agitate/core/branch.lua
@@ -1,3 +1,12 @@
 local M = {}
 
+function M.CreateCheckoutAndPush(branch_name)
+    if branch_name ~= nil and branch_name ~= '' then
+        vim.cmd('G checkout -b ' .. branch_name)
+        vim.cmd('G push -u origin ' .. branch_name)
+    else
+       print('Please provide an argument for the name of the branch')
+    end
+end
+
 return M
