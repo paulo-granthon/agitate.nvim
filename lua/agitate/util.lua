8b4a48e6194b3de910d32e1be64ee62a9b86c10d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 18:28:54 -0300

    feat: `flatten_table` function in `util`

--- lua/agitate/util.lua
+++ lua/agitate/util.lua
@@ -5,10 +5,32 @@ function M.get_directory_name()
     return vim.fn.getcwd():match("^.+/(.+)$")
 end
 
-
 -- Function to execute shell commands
 function M.execute_command(command)
     return vim.fn.systemlist(command)
+end
+
+---@class FlattenTableOptions
+---@field skip? number The number of lines to skip before constructing the table
+local flatten_table_opts_example = {
+    skip = 0
+}
+
+---Flattens a table of String into a single String
+---@param table table the table to flatten
+---@param opts? FlattenTableOptions the optional options table
+function M.flatten_table(table, opts)
+    local result = ''
+    local skip = opts and opts.skip or 0
+    for _, line in ipairs(table) do
+        if skip > 0 then
+            skip = skip - 1
+            goto continue
+        end
+        result = result .. line
+        ::continue::
+    end
+    return result
 end
 
 return M
