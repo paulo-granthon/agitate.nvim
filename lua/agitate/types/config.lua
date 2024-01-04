237bc809e32b45b5db1ce3330a0829a38c0408a2
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-04 01:10:01 -0300

    feat: add `github_access_token` field to `Config` class in `types.config`

--- lua/agitate/types/config.lua
+++ lua/agitate/types/config.lua
@@ -3,4 +3,5 @@
 
 ---@class Config
 ---@field github_username? string|nil Your GitHub profile's username
+---@field github_access_token? string|nil Your GitHub profile's PAT (Personal Access Token)
 ---@field repo? RepositoryConfig Options related to the `repository` context
