f3c3cda845cef64fd14af587c7a7bffd39215bff
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-04 17:36:22 -0300

    feat: add `const.error` module for error messages

---
+++ lua/agitate/const/error.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+---@type string
+M.import = 'Error importing `agitate.error` module.'
+
+---@type string
+M.err_types = 'Error importing `agitate.types.error` module inside `agitate.error`.'
+
+return M
