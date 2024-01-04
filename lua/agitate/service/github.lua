958be2cd4e134923d735e55b57a7748308f06452
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-04 00:49:38 -0300

    feat: throw `AgitateError` instead of `string` in `post_new_repo` function of `service.github`

--- lua/agitate/service/github.lua
+++ lua/agitate/service/github.lua
@@ -1,13 +1,14 @@
 local M = {}
 
 local util = require('agitate.util')
+local error = require('agitate.error')
 require('types.github')
 
 ---Creates a new remote repository through the GitHub API
 ---@param access_token string Your GitHub PAT (Personal Access Token)
 ---@param repository string Name of the repository to be created
 ---@return boolean Ok If proccess was executed successfully
----@return GitHubSuccessResponse|GitHubErrorResponse|string Response
+---@return GitHubSuccessResponse|GitHubErrorResponse|AgitateError Response
 ---Response properly formatted for the rest of `agitated.nvim`
 function M.post_new_repo(access_token, repository)
     -- Execute curl to create the repository through the GitHub api
@@ -25,7 +26,7 @@ function M.post_new_repo(access_token, repository)
 
     -- Trim any noise left of the first `{` or right of the last `}`
     local ok, gsubbed_github_response = util.json_lr_trim(flattened_github_response)
-    if not ok then return ok, 'Unhandled error at `service.github.post_new_repo`' end
+    if not ok then return ok, error.unhandled('service.github.post_new_repo') end
 
     -- Return the processed response as a lua table
     return true, vim.json.decode(gsubbed_github_response)
