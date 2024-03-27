dbd98965b4b7c0749fea8c56360aef8d05b7d2b2
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-27 00:55:25 -0300

    feat: create `get_organization` function in `service.github`

--- lua/agitate/service/github.lua
+++ lua/agitate/service/github.lua
@@ -55,4 +55,32 @@ function M.post_new_repo(access_token, repository)
   return true, vim.json.decode(repo_json)
 end
 
+---Get information about an organization on GitHub
+---@param access_token string Your GitHub PAT (Personal Access Token)
+---@param organization string Name of the organization to get information about
+---@return boolean Ok If proccess was executed successfully
+---@return GitHubGetOrgSuccessResponse|GitHubErrorResponse|AgitateError Response
+---Response properly formatted for the rest of `agitated.nvim`.
+---Might contain the organization information relavant to the rest of Agitate or an error message
+---@see GitHubGetOrgSuccessResponse
+---@see GitHubErrorResponse
+---@see AgitateError
+function M.get_organization(access_token, organization)
+  -- Execute curl to get the organization information through the GitHub api
+  local raw_github_response =
+    util.execute_command('curl' .. ' -H ' .. '"Authorization: token ' .. access_token .. '"' .. ' https://api.github.com/orgs/' .. organization)
+
+  -- Flatten the table response to string
+  local flattened_github_response = util.flatten_table(raw_github_response)
+
+  -- Trim any noise left of the first `{` or right of the last `}`
+  local json_lr_trim_ok, org_json = util.json_lr_trim(flattened_github_response)
+  if not json_lr_trim_ok then
+    return json_lr_trim_ok, error.unhandled('service.github.get_organization')
+  end
+
+  -- Return the processed response as a lua table
+  return true, vim.json.decode(org_json)
+end
+
 return M
