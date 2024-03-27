91733b56b73a8a64e0cc08fde2995efe93a20561
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-27 00:44:57 -0300

    feat: create `GitHubGetOrgSuccessResponse` type

--- lua/agitate/types/github.lua
+++ lua/agitate/types/github.lua
@@ -1,6 +1,10 @@
 ---@class GitHubNewRepoSuccessResponse
 ---@field html_url string The URL of the repository
 
+---@class GitHubGetOrgSuccessResponse
+---@field name string The full name of the organization
+---@field repos_url string The URL of the organization's repositories
+
 ---@class GitHubErrorResponse
 ---@field errors GitHubError[] Errors during the request
 
