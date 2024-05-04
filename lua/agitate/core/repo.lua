75c897ed73b42890b19fb430b97ffce80bbf6f43
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-04 14:46:43 -0300

    fix: also error out if `html_url` is not in `github_post_response` in `CreateGitHubCurl` function of `core.repo`

--- lua/agitate/core/repo.lua
+++ lua/agitate/core/repo.lua
@@ -60,7 +60,7 @@ function M.CreateGitHubCurl(optional_parameters)
     error.throw(github_post_response)
   end
 
-  if github_post_response.errors then
+  if github_post_response.errors or not github_post_response.html_url then
     return vim.api.nvim_err_writeln(
       'Agitate | CreateGitHubCurl | Error:'
         .. '\nFailed to create repository at '
